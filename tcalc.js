3dd1ded23f84da23d75875ef8176c23729c48302
Justin <justin.hellings@gmail.com>
2011-06-12 22:41:41 +0100

    Added copyright notice. (Could get used to this.)

--- tcalc.js
+++ tcalc.js
@@ -1,5 +1,29 @@
+
+/**
+ * tcalc.js - Perform calculations with times in hh:mm format.
+ * 
+ * NOTE: I've tested this script using rhino cli, hence the java I/O code.
+ * 
+ * Copyright 2011 Justin Hellings justin@ecobee.org/justin.hellings@gmail.com
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Seem to need to pull in java packages to provide IO :-(
 importPackage(java.io);
 importPackage(java.lang);
+
 
 function to_hours( hr_mn ) {
   components = hr_mn.split( ':', 2 );
